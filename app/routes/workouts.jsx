c2ea42dfe1cd85ac6c26fde37131b21b75d60c4d
JohnKazantzis <gkazantzis96@gmail.com>
2025-05-13 08:31:28 +0300

    workouts page updates. Added responsiveness

--- app/routes/workouts.jsx
+++ app/routes/workouts.jsx
@@ -2,12 +2,14 @@ import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import { getWorkoutsByUserId } from '../services/workoutService';
 
+const DEFAULT_PAGE = '0';
+const PAGINATION_PAGE_SIZE = 10;
+
 export async function loader({ request }) {
     const url = new URL(request.url);
-    const page = url.searchParams.get('page') || '0';
+    const page = url.searchParams.get('page') || DEFAULT_PAGE;
 
-    const workouts = await getWorkoutsByUserId(page, 10, 5);
-    
+    const workouts = await getWorkoutsByUserId(page, PAGINATION_PAGE_SIZE, 5);
     
     return json(workouts.data);
 }
@@ -17,10 +19,10 @@ export default function Workouts() {
     console.log(workouts);
     
     return(
-        <div className='flex flex-wrap gap-2 justify-center items-start'>
+        <div className='flex flex-col gap-2 justify-center items-center'>
             { workouts.content.map(workout => {
                 return(
-                    <div className='flex flex-col justify-between p-2 w-48 h-48 bg-red-400 rounded' key={workout.id}>
+                    <div className='flex flex-col justify-between p-2 w-4/5 h-24 bg-red-400 rounded hover:bg-red-600 md:w-3/5' key={workout.id}>
                         <div className='text-gray-900 font-bold'>{(new Date(workout.workoutDate)).toLocaleDateString()}</div>
                         <div className='flex flex-wrap gap-0.5'>
                             {workout.muscleGroups && workout.muscleGroups.map((muscleGroup) => 
