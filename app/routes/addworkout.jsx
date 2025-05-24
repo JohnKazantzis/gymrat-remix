e85c2d90ac131242f6c51853d4dd52d06d1b1c1d
JohnKazantzis <gkazantzis96@gmail.com>
2025-05-24 16:12:58 +0300

    Finished addworkout route UI

--- app/routes/addworkout.jsx
+++ app/routes/addworkout.jsx
@@ -3,12 +3,20 @@ import { useState } from 'react';
 
 export default function AddWorkout() {
     const [exercisesByMuscleGroup, setExercisesByMuscleGroup] = useState({
-        'Chest': {key: 1, exercises: ['Bench Press', 'Pec Dec', 'Dumbbel Bench Press', 'Incline Bench Press']},
-        'Back': {key: 2, exercises: ['Pull-up', 'Lat Pull Down', 'Rows']},
-        'Legs': {key: 3, exercises: ['Leg Press', 'Squat', 'Deadlift', 'Quad Extensions']}
+        'Chest': {key: 1, exercises: [{key: 1, name: 'Bench Press', muscleGroup: 'Chest'}, {key: 2, name: 'Pec Dec', muscleGroup: 'Chest'}, {key: 3, name: 'Dumbbel Bench Press', muscleGroup: 'Chest'}, {key: 4, name: 'Incline Bench Press', muscleGroup: 'Chest'}]},
+        'Back': {key: 2, exercises: [{key: 5, name: 'Pull-up', muscleGroup: 'Back'}, {key: 6, name: 'Lat Pull Down', muscleGroup: 'Back'}, {key: 7, name: 'Rows', muscleGroup: 'Back'}]},
+        'Legs': {key: 3, exercises: [{key: 8, name: 'Leg Press', muscleGroup: 'Legs'}, {key: 9, name: 'Squat', muscleGroup: 'Legs'}, {key: 10, name: 'Deadlift', muscleGroup: 'Legs'}, {key: 11, name: 'Quad Extensions', muscleGroup: 'Legs'}]}
     });
+    const [selectedMuscle, setSelectedMuscle] = useState(Object.keys(exercisesByMuscleGroup).length > 0 ? Object.keys(exercisesByMuscleGroup)[0] : null);
+    const [selectedExercises, setSelectedExercises] = useState([{key: 1, name: 'Bench Press', muscleGroup: 'Chest'}]);
 
-    
+    const toogleExercise = exercise => {
+        if(selectedExercises.find(item => item.key == exercise.key)) {
+            setSelectedExercises(selectedExercises.filter(item => item.key !== exercise.key));
+            return;
+        }
+        setSelectedExercises([...selectedExercises, exercise]);
+    }
     
     return(
         <>
@@ -21,29 +29,37 @@ export default function AddWorkout() {
                     </div>
                     <div className='flex flex-row'>
                         <p className='text-white'>Search in</p>
-                        <select name='musclegroups' className='bg-gray-900 text-white cursor-pointer'>
-                            <option value='Abdominals'>Abdominals</option>
-                            <option value='Legs'>Legs</option>
-                            <option value='Biceps'>Biceps</option>
-                            <option value='Shoulders'>Shoulders</option>
-                            <option value='Chest'>Chest</option>
-                            <option value='Back'>Back</option>
-                            <option value='Triceps'>Triceps</option>
-                            <option value='Traps'>Traps</option>
-                            <option value='Forearms'>Forearms</option>
-                            <option value='Neck'>Neck</option>
+                        <select name='musclegroups' defaultValue={selectedMuscle} onChange={(event) => setSelectedMuscle(event.target.value)} className='bg-gray-900 text-white cursor-pointer'>
+                            { exercisesByMuscleGroup && Object.keys(exercisesByMuscleGroup).map(muscle => 
+                                <option key={muscle} value={muscle}>
+                                    {muscle}
+                                </option>
+                            )}
                         </select>
                     </div>
                 </div>
                 <div className='flex flex-row flex-wrap gap-1 px-2 md:px-0'>
                     { exercisesByMuscleGroup && Object.keys(exercisesByMuscleGroup).map((muscle) => 
-                        <div key={muscle} className='flex flex-row items-center gap-2 py-1 px-2 rounded bg-brink-pink-400'>
+                        <div key={muscle} className='flex flex-row justify-between items-center gap-2 py-1 px-2 rounded bg-brink-pink-400'>
                             <span>{muscle}</span>
-                            <span className='text-white text-xs'>2</span>                    
+                            <span className='text-white text-xs'>{selectedExercises.reduce((accumulator, exercise) => exercise.muscleGroup === muscle ? accumulator + 1 : accumulator, 0)}</span>                    
                         </div>)
                     }
                 </div>
-                <div></div>
+                <div className='flex flex-col gap-1'>
+                    { exercisesByMuscleGroup && exercisesByMuscleGroup[selectedMuscle].exercises.map(exercise => 
+                        <button 
+                            key={exercise.key} 
+                            className='h-10 flex flex-row justify-between items-center bg-white rounded py-1 px-2 cursor-pointer hover:bg-brink-pink-400'
+                            onClick={ () => toogleExercise(exercise) }>
+                            <span>{exercise.name}</span>
+                            {   
+                                selectedExercises.find(item => item.key === exercise.key) ?
+                                <img src='/icons8-check-mark-50.png' alt='checkmark' className='w-8 h-8'></img> : <></>
+                            }
+                        </button>    
+                    )}
+                </div>
             </div>
         </>
     );
